9d94c7d07534118b5673c4d190a25d8fe19e25dc
Ravindu <fernandoravindu1994@gmail.com>
2016-03-19 14:33:19 +0530

    Created sample test.

--- EVote_Mobile/MainPage.xaml.cs
+++ EVote_Mobile/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -44,5 +46,28 @@ namespace EVote_Mobile
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            CallService();
+        }
+
+        private async void CallService()
+        {
+            // Create HttpClient
+            HttpClient httpClient = new System.Net.Http.HttpClient();
+
+            // Do the request 
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3402/VoteServices.svc/poll/findall");
+
+            // Get the response
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            // Read the response (here a string)
+            string data = await response.Content.ReadAsStringAsync();
+
+            textBlock.Text = data.ToString();
+        }
+
     }
 }
